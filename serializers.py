2e50b5445d58fc63c38c0de4eef6dbc52fce79e8
gofven <cratedcreations@gmail.com>
2024-01-31 15:21:13 +0100

    Fixed AccountSerializer

--- serializers.py
+++ serializers.py
@@ -5,6 +5,6 @@ from flowback.user.serializers import BasicUserSerializer
 class AccountSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     account_number = serializers.CharField()
-    account_name = serializers.CharField
+    account_name = serializers.CharField()
 
     created_by = BasicUserSerializer(source='user')
